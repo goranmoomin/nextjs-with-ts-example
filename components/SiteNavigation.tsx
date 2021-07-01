c1af6711cb97bdbd0b8b9c24766c4c6df2dc8193
Kijun Shin <sharaelong.shin@gmail.com>
2021-07-01 13:49:10 +0900

    Implement siteNavigation element

---
+++ components/SiteNavigation.tsx
@@ -0,0 +1,78 @@
+import { InlineSpacer } from "./Spacer";
+
+export declare interface SiteNavigationProps {
+  href: string;
+  label: string;
+  body: string;
+  children: React.ReactNode;
+}
+
+export function SiteNavigation({
+  href,
+  label,
+  body,
+  children,
+}: SiteNavigationProps) {
+  return (
+    <a href={href} className="site-navigation-container">
+      <span className="site-navigation-icon-container">{children}</span>
+      <InlineSpacer size={14} axis="horizontal" />
+      <span className="menuitem-container">
+        <span className="menuitem-label">{label}</span>
+        <svg width="10" height="10" aria-hidden="true" viewBox="0 0 10 10">
+          <g fillRule="evenodd">
+            <path d="M0 5h6.5" className="line-path"></path>
+            <path d="M1 1.5L4.25 5 1 8.5" className="tip-path"></path>
+          </g>
+        </svg>
+        <p className="menuitem-body">{body}</p>
+      </span>
+      <style jsx>{`
+        .site-navigation-container {
+          display: inline-flex;
+          height: 40px;
+          align-items: center;
+        }
+        .site-navigation-icon-container {
+          width: 32px;
+          height: 32px;
+        }
+        .menuitem-label {
+          font-size: 14px;
+          font-weight: 480;
+        }
+        .menuitem-body {
+          margin: 0;
+          font-size: 13px;
+          opacity: 0.6;
+          transition: opacity 150ms ease;
+        }
+        svg {
+          stroke-width: 1.5px;
+          fill: none;
+          stroke: currentColor;
+          margin-left: 8px;
+        }
+        .line-path {
+          opacity: 0;
+          transition: opacity 150ms ease;
+        }
+        .tip-path {
+          opacity: 0;
+          transition: opacity 150ms ease;
+          transition: transform 150ms ease;
+        }
+        .site-navigation-container:hover .line-path {
+          opacity: 1;
+        }
+        .site-navigation-container:hover .tip-path {
+          opacity: 1;
+          transform: translateX(3px);
+        }
+        .site-navigation-container:hover .menuitem-body {
+          opacity: 1;
+        }
+      `}</style>
+    </a>
+  );
+}
---
+++ pages/test.tsx
@@ -0,0 +1,62 @@
+import { ReactHTMLElement } from "react";
+import { SiteNavigation } from "../components/SiteNavigation";
+
+export default function test() {
+  return (
+    <>
+      <SiteNavigation href="/" label="Payments" body="Online Payments">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="none"
+          className="ProductIcon ProductIcon--Payments"
+          viewBox="0 0 40 40"
+        >
+          <path
+            fill="url(#product-icon-payments-SiteMenu-a)"
+            d="M34.61 11.28a2.56 2.56 0 00-1.22-1.04L8.54.2A2.57 2.57 0 005 2.6V15c0 1.05.64 2 1.61 2.4l6.44 2.6 21.56 8.72c.26-.4.4-.88.39-1.36V12.64c0-.48-.13-.96-.39-1.37z"
+          ></path>
+          <path
+            fill="#96F"
+            d="M34.63 11.28L13.06 20l-6.45 2.6A2.58 2.58 0 005 25v12.42a2.58 2.58 0 003.54 2.39L33.4 29.76c.5-.21.93-.57 1.21-1.04.26-.41.4-.88.39-1.36V12.64c0-.48-.12-.95-.37-1.36z"
+          ></path>
+          <path
+            fill="url(#product-icon-payments-SiteMenu-b)"
+            d="M34.62 11.28l.1.17c.18.37.28.77.28 1.19v-.03 14.75c0 .48-.13.95-.39 1.36L13.06 20l21.56-8.72z"
+          ></path>
+          <defs>
+            <linearGradient
+              id="product-icon-payments-SiteMenu-a"
+              x1="20"
+              x2="20"
+              y1="4.13"
+              y2="21.13"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#11EFE3"></stop>
+              <stop offset="1" stopColor="#21CFE0"></stop>
+            </linearGradient>
+            <linearGradient
+              id="product-icon-payments-SiteMenu-b"
+              x1="35"
+              x2="35"
+              y1="11.28"
+              y2="28.72"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#0048E5"></stop>
+              <stop offset="1" stopColor="#9B66FF"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+      </SiteNavigation>
+      <style jsx>{`
+        svg {
+          width: 100%;
+          height: 100%;
+        }
+      `}</style>
+    </>
+  );
+}
